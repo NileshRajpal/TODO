643a0e3dfd33a927817d0ff167170d0c3eadd0a8
Nilesh Rajpal <nileshrajpal7@gmail.com>
2018-09-20 14:49:51 +0530

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,7 +1,7 @@
 <?php
 $time_zone = 'Asia/Kolkata';
 
-$con = mysqli_connect("localhost", "saikat", "ppioneer", "todo_db");
+$con = mysqli_connect("localhost", "bright1999", "bright1999", "bright1999");
 
 if(!$con) {
     echo "Error connecting to MySQL <br>";
